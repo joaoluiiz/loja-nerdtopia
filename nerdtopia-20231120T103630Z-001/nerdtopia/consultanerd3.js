e41a8258a5655fd613b83a30600ccd8a27df82f6
cauarlopes <147401480+cauarlopes@users.noreply.github.com>
2023-11-20 08:30:57 -0300

    loja nerdtopia
    loja atualizada

---
+++ nerdtopia-20231120T103630Z-001/nerdtopia/consultanerd3.js
@@ -0,0 +1,46 @@
+var dataRef = new Firebase('https://nerdtopia-dc82d-default-rtdb.firebaseio.com');
+
+dataRef.on('value', function(snapshot) {
+
+    var t = snapshot.val();
+    var count = 0;
+
+    for (var key in t) {
+        var valor = t[key];
+
+        console.log('chave', key);
+        console.log('valor', valor);
+
+        if(key === 'bonecos') {
+
+            var _itemsKey = Object.entries(valor);
+    
+    
+    
+            for (const [key2, value2] of _itemsKey) {
+                count++;
+    
+                $("#tb tbody").append(
+    
+                    `<tr> +
+    
+                        <th scope="row">${count}</th> +
+    
+                        <td>${value2.nome}</td> +
+    
+                        <td>${value2.preco}</td> +
+    
+                        <td>${value2.tamanho}</td> +
+
+    
+                    </tr>`
+    
+                );
+    
+            }
+        }
+
+
+    }
+
+});
